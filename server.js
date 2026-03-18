d891bf6abf4a330ff229e342f2719847d74d3dbd
jsbowen79 <jsbowen79@outlook.com>
2026-03-18 10:48:27 -0600

    Week3 Learning Activities completed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const utilities = require("./utilities/index.js")
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
@@ -25,10 +26,31 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index Route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 //Inventory Route
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'That car was just too fast for you.  The insurance would break you.  Try looking for a different car!'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
