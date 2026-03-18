f00a3bde0c8ac278e220be9d5b0aa895dcb71dcd
jsbowen79 <jsbowen79@outlook.com>
2026-03-17 21:35:09 -0600

    week 3--works, needs CSS

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute.js")
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -24,9 +25,10 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index Route
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
+//Inventory Route
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
